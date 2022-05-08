a4d2172eecea5e6d476ee77db8b0b97f55175310
Urieluno <urielperrenfernandez@gmail.com>
2022-05-08 15:49:55 -0300

    Solucionando problemas con las imagenes2

--- app.js
+++ app.js
@@ -9,35 +9,35 @@ app.listen(3030, () => {
 
 // Rutas
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/index.html'));
+    res.sendFile(path.join(__dirname, 'DH-Heroes/views/index.html'));
 });
 
 app.get('/babbage', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/babbage.html'));
+    res.sendFile(path.join(__dirname, 'DH-Heroes/views/babbage.html'));
 });
 
 app.get('/berners-lee', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/berners-lee.html'));
+    res.sendFile(path.join(__dirname, 'DH-Heroes/views/berners-lee.html'));
 });
 
 app.get('/clarke', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/clarke.html'));
+    res.sendFile(path.join(__dirname, 'DH-Heroes/views/clarke.html'));
 });
 
 app.get('/hamilton', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/hamilton.html'));
+    res.sendFile(path.join(__dirname, 'DH-Heroes/views/hamilton.html'));
 });
 
 app.get('/hopper', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/hopper.html'));
+    res.sendFile(path.join(__dirname, 'DH-Heroes/views/hopper.html'));
 });
 
 app.get('/lovelace', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/lovelace.html'));
+    res.sendFile(path.join(__dirname, 'DH-Heroes/views/lovelace.html'));
 });
 
 app.get('/turing', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/turing.html'));
+    res.sendFile(path.join(__dirname, 'DH-Heroes/views/turing.html'));
 });
 
 // Archivos static
